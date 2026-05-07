49d735c4d41ad0441d08dec60dbd6540e3f43235
Tekalegn Mekonen <tekamek25@gmail.com>
2026-05-07 14:50:11 +0300

    chore: add core dependencies for state management, networking, and storage

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,13 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  firebase_core
+  flutter_secure_storage_windows
+  smart_auth
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
